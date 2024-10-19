86850970c7a6cfd93073236f43aee2ce7bc22b20
Ehsan Bikdeli <ebikdeli@gmail.com>
2024-10-19 15:58:52 +0330

    clean code updated

--- _main.py
+++ _main.py
@@ -22,12 +22,15 @@ def main():
         print(f'"{joblisting_module}" is not in the joblisting directory')
         return -1
     # ! 2- Call the joblising website module
-    module = import_module(f'joblistings.{joblisting_module}')
-    result = module.extract_website(site_url)
-    print(result)
-    
-    
-# main()
+    try:
+        module = import_module(f'joblistings.{joblisting_module}')
+        ew = module.ExtractWebsite(site_url)
+        result = ew.start()
+        print(result)
+    except Exception as e:
+        print(f'Error: Cannot extract data from "{joblisting_module}"')
+
+main()
 
 
 ################################################################################### !
@@ -36,15 +39,15 @@ def main():
 ################################################################################### !
 ################################################################################### !
 
-from drivers.selenium_driver import call_selenium_driver
-from joblistings.jobvision_ir import ExtractJob
-import time
-job_url = 'file:///C:/CODES/PYTHON/python-iran-hot-jobs/_resources/%D8%A7%D8%B3%D8%AA%D8%AE%D8%AF%D8%A7%D9%85%20%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D9%87_%D9%86%D9%88%DB%8C%D8%B3%20%D8%A7%D8%B1%D8%B4%D8%AF%20Python%20%D8%AF%D8%B1%20%D8%B4%D8%B1%DA%A9%D8%AA%20%D8%AC%D8%B1%D8%A7%D8%AD%20%DB%8C%D8%A7%D8%B1%D8%A7%D9%86%20%D9%BE%D8%A7%D8%B1%D8%B3%DB%8C%D8%A7%D9%86%20_%20%D8%AC%D8%A7%D8%A8_%D9%88%DB%8C%DA%98%D9%86%20(10_15_2024%203_48_02%20PM).html'
+# from drivers.selenium_driver import call_selenium_driver
+# from joblistings.jobvision_ir import ExtractJob
+# import time
+# job_url = 'file:///C:/CODES/PYTHON/python-iran-hot-jobs/_resources/%D8%A7%D8%B3%D8%AA%D8%AE%D8%AF%D8%A7%D9%85%20%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D9%87_%D9%86%D9%88%DB%8C%D8%B3%20%D8%A7%D8%B1%D8%B4%D8%AF%20Python%20%D8%AF%D8%B1%20%D8%B4%D8%B1%DA%A9%D8%AA%20%D8%AC%D8%B1%D8%A7%D8%AD%20%DB%8C%D8%A7%D8%B1%D8%A7%D9%86%20%D9%BE%D8%A7%D8%B1%D8%B3%DB%8C%D8%A7%D9%86%20_%20%D8%AC%D8%A7%D8%A8_%D9%88%DB%8C%DA%98%D9%86%20(10_15_2024%203_48_02%20PM).html'
 # job_url = 'file:///C:/CODES/PYTHON/python-iran-hot-jobs/_resources/%D8%A7%D8%B3%D8%AA%D8%AE%D8%AF%D8%A7%D9%85%20Data%20Scientist%20%D8%AF%D8%B1%20%D8%B4%D8%A7%D9%BE%DB%8C%D9%86%D9%88%20_%20%D8%AC%D8%A7%D8%A8_%D9%88%DB%8C%DA%98%D9%86%20(10_19_2024%2012_24_29%20PM).html'
 # job_url = 'file:///C:/CODES/PYTHON/python-iran-hot-jobs/_resources/%D8%A7%D8%B3%D8%AA%D8%AE%D8%AF%D8%A7%D9%85%20Full%20Stack%20Software%20Engineer%20(AI%20Developer)%20%D8%AF%D8%B1%20%D9%85%D9%88%D8%B3%D8%B3%D9%87%20%D9%85%D9%87%D8%A7%D8%AC%D8%B1%D8%AA%DB%8C%20applyland%20_%20%D8%AC%D8%A7%D8%A8_%D9%88%DB%8C%DA%98%D9%86%20(10_19_2024%2012_24_35%20PM).html'
-driver = call_selenium_driver(headless=1)
-driver.get(job_url)
-time.sleep(2)
-ej = ExtractJob(driver=driver, job_url=job_url)
-res = ej.start_extraction(job_describtion=False)
-print(res)
+# driver = call_selenium_driver(headless=1)
+# driver.get(job_url)
+# time.sleep(2)
+# ej = ExtractJob(driver=driver, job_url=job_url)
+# res = ej.start_extraction(job_describtion=False)
+# print(res)
--- joblistings/jobvision_ir.py
+++ joblistings/jobvision_ir.py
@@ -8,107 +8,115 @@ import pathlib
 import os
 
 
-def extract_website(site_url: str, job_title: str='python') -> str:
-    """Extract jobvision.ir jobs"""
-    print(f'Call the joblisting website for the "{site_url}"')
-    try:
-        if _is_check_job_links_file_date():
-            print('Just today jobvision jobs scraped')
-            return 'No need to scrapp jobvision'
-        driver = call_selenium_driver(headless=1)
-        job_link_set = set()
-        page_number = 1
-        # ! Get all the 'job_title' jobs in jobvision search
-        job_link_set = _get_job_links(driver, job_title, job_link_set, page_number)
-        # ! Process the every job links extracted
-        print(f'Number of jobs found for "{job_title}" job title: {len(job_link_set)}')
-        # To not repeat the above process again and save resources for a day, save the extracted job links into a file
-        if not _write_link_into_file(job_link_set):
-            driver.close()
-            return 'Could not enter job links into related file'
-        # Extracted all specified data from every job page link in the 'job_link_set'
-        # !!!!!!!!!!!!
-        # for job_url in job_link_set:
-        #     print(f'Extracting link:\n{job_url}')
-        #     ej = ExtractJob(driver=driver, job_url=job_url)
-        #     ej.start_extraction(job_describtion=False)
-        # !!!!!!!!!!!
-    except Exception as e:
-        print(f'CANNOT OPEN "{site_url}"')
-        print(e.__str__())
-        # return -1
-    driver.close()
-    return 'OK'
-
-
-def _get_job_links(driver:WebDriver, job_title:str, job_link_set:set, page_number:int, _job_card_css_selector:str='job-card > a', _pagination_css_selector:str='.pagination-page.page-item.active') -> set:
-    """Get job links in jobvision search section"""
-    # ! Add job links to the job_link_set
-    try:
-        while True:
-            print('page number: ', page_number)
-            search_url = f'https://jobvision.ir/jobs/keyword/{job_title}?page={page_number}'
-            driver.get(search_url)
-            sleep(1)
-            for jb in driver.find_elements(By.CSS_SELECTOR, _job_card_css_selector):
-                job_link_set.add(jb.get_attribute('href'))
-            # ! Check if there are any page remain to being scraped
-            try:
-                driver.find_element(By.CSS_SELECTOR, _pagination_css_selector)
-                page_number += 1
-            except NoSuchElementException:
-                print(f'No element found as "{_pagination_css_selector}" or no page remains')
-                return job_link_set
-            except Exception as e:
-                print(e.__str__())
-                return job_link_set
-    except Exception as e:
-        print(f'Problem in scraping below url:\n{search_url}\nError:\n{e.__str__()}')
-        return job_link_set
-
-
-def _is_check_job_links_file_date() -> bool:
-    """Check the date in the first line of the '_jobvision_links.txt'. If less than a day passed since the job scrapped, return True. If no file found, create a new file"""
-    current_directory = pathlib.Path(__file__).parent.resolve()
-    job_link_file_name = "_jobvision_links.txt"
-    try:
-        with open(f'{os.path.join(current_directory, job_link_file_name)}', 'rt') as f:
-            link_sample = f.readlines(400)
-            date_str = link_sample[0]
-        date_obj = datetime.date.fromisoformat(date_str.replace('\n', ''))
-        td = datetime.date.today() - date_obj
-        if td.days == 0 and len(link_sample) > 2:
-            return True
-    # If the file does not exist create it and enter today date in the first line in the file
-    except Exception:
-        with open(f'{os.path.join(current_directory, job_link_file_name)}', 'wt') as f:
-            f.write(datetime.date.today().strftime('%Y-%m-%d'))
-        print('Jobvision job list file just created')
-    return False
-
-
-def _write_link_into_file(job_link_set:set) -> bool:
-    """Write job link urls into job_link file. If successful return True"""
-    try:
+class ExtractWebsite:
+    """Extract data from jobvision.ir website."""
+    def __init__(self, site_url:str, job_title:str='python') -> None:
+        self.site_url = site_url
+        self.job_title = job_title
+        try:
+            driver = call_selenium_driver(headless=1)
+        except Exception as e:
+            print('Problem with selenium:\n', e.__str__())
+            return None
+        self.driver = driver
+    
+    def start(self) -> str:
+        """Extract jobvision.ir jobs"""
+        print(f'Call the joblisting website for the "{self.site_url}"')
+        try:
+            if self._is_check_job_links_file_date():
+                print('Just today jobvision jobs scraped')
+                return 'No need to scrapp jobvision'
+            job_link_set = set()
+            page_number = 1
+            # ! Get all the 'job_title' jobs in jobvision search
+            job_link_set = self._get_job_links(job_link_set, page_number)
+            # ! Process the every job links extracted
+            print(f'Number of jobs found for "{self.job_title}" job title: {len(job_link_set)}')
+            # To not repeat the above process again and save resources for a day, save the extracted job links into a file
+            if not self._write_link_into_file(job_link_set):
+                self.driver.close()
+                return 'Could not enter job links into the related file'
+            # Extracted all specified data from every job page link in the 'job_link_set'
+            # !!!!!!!!!!!!
+            # for job_url in job_link_set:
+            #     print(f'Extracting link:\n{job_url}')
+            #     ej = ExtractJob(driver=driver, job_url=job_url)
+            #     ej.start_extraction(job_describtion=False)
+            # !!!!!!!!!!!
+        except Exception as e:
+            print(f'CANNOT OPEN "{self.site_url}"')
+            print(e.__str__())
+            return 'Extraction failed'
+        self.driver.close()
+        return 'OK'
+    
+    def _get_job_links(self, job_link_set:set, page_number:int, _job_card_css_selector:str='job-card > a', _pagination_css_selector:str='.pagination-page.page-item.active') -> set:
+        """Get job links in jobvision search section"""
+        # ! Add job links to the job_link_set
+        try:
+            while True:
+                print('page number: ', page_number)
+                search_url = f'https://jobvision.ir/jobs/keyword/{self.job_title}?page={page_number}'
+                self.driver.get(search_url)
+                sleep(1)
+                for jb in self.driver.find_elements(By.CSS_SELECTOR, _job_card_css_selector):
+                    job_link_set.add(jb.get_attribute('href'))
+                # ! Check if there are any page remain to being scraped
+                try:
+                    self.driver.find_element(By.CSS_SELECTOR, _pagination_css_selector)
+                    page_number += 1
+                except NoSuchElementException:
+                    print(f'No element found as "{_pagination_css_selector}" or no page remains')
+                    return job_link_set
+                except Exception as e:
+                    print(e.__str__())
+                    return job_link_set
+        except Exception as e:
+            print(f'Problem in scraping below url:\n{search_url}\nError:\n{e.__str__()}')
+            return job_link_set
+    
+    def _is_check_job_links_file_date(self) -> bool:
+        """Check the date in the first line of the '_jobvision_links.txt'. If less than a day passed since the job scrapped, return True. If no file found, create a new file"""
         current_directory = pathlib.Path(__file__).parent.resolve()
         job_link_file_name = "_jobvision_links.txt"
-        with open(f'{os.path.join(current_directory, job_link_file_name)}', 'wt') as f:
-            f.write(datetime.date.today().strftime('%Y-%m-%d'))
-            for ul in job_link_set:
-                f.write(f"{'\n\n' + ul}")
-        print('Successfully entered job urls into file')
-        return True
-    except Exception as e:
-        print('A problem happend:\n', e.__str__())
+        try:
+            with open(f'{os.path.join(current_directory, job_link_file_name)}', 'rt') as f:
+                link_sample = f.readlines(400)
+                date_str = link_sample[0]
+            date_obj = datetime.date.fromisoformat(date_str.replace('\n', ''))
+            td = datetime.date.today() - date_obj
+            if td.days == 0 and len(link_sample) > 2:
+                return True
+        # If the file does not exist create it and enter today date in the first line in the file
+        except Exception:
+            with open(f'{os.path.join(current_directory, job_link_file_name)}', 'wt') as f:
+                f.write(datetime.date.today().strftime('%Y-%m-%d'))
+            print('Jobvision job list file just created')
         return False
+    
+    def _write_link_into_file(self, job_link_set:set) -> bool:
+        """Write job link urls into job_link file. If successful return True"""
+        try:
+            current_directory = pathlib.Path(__file__).parent.resolve()
+            job_link_file_name = "_jobvision_links.txt"
+            with open(f'{os.path.join(current_directory, job_link_file_name)}', 'wt') as f:
+                f.write(datetime.date.today().strftime('%Y-%m-%d'))
+                for ul in job_link_set:
+                    f.write(f"{'\n\n' + ul}")
+            print('Successfully entered job urls into file')
+            return True
+        except Exception as e:
+            print('A problem happend:\n', e.__str__())
+            return False
 
 
 class ExtractJob:
-    """In job page, extract all data from a single_page page"""
-    def __init__(self, driver: WebDriver, job_url: str):
+    """In job page, extract all data from a single_page page."""
+    def __init__(self, driver: WebDriver, job_url: str) -> None:
         self.driver = driver
         self.job_url = job_url
-
+        
     def start_extraction(self, title:bool=True, company_name:bool=True, company_link:bool=True,
                         salary:bool=True, experience:bool=True, age:bool=True, education:bool=True,
                         skills:bool=True, gender:bool=True, language:bool=True, job_describtion:bool=True) -> dict:
