4bbffbc61c62c2c89b43d0d02ba40c43ce59f147
Ehsan Bikdeli <ebikdeli@gmail.com>
2024-10-20 00:13:13 +0330

    export data structured better

--- save_data/save_csv.py
+++ save_data/save_csv.py
@@ -6,12 +6,12 @@ def into_csv(data:dict, url:str) -> bool:
     """Save extracted data into a csv file"""
     try:
         csv_file = f'{os.getcwd()}/jobs.csv'
-        fields = ['title', 'url', 'company_name', 'company_link',
-                'salary', 'experience', 'age', 'gender',
-                'language', 'skills', 'education', 'description']
-        data_row = [data['title'], url, data['company_name'], data['company_link'],
-                    data['salary'], data['experience'], data['age'], data['education'],
-                    data['skills'], data['gender'], data['language'], data['job_description']]
+        fields = ['title', 'url', 'company_name', 'company_link', 'salary_min', 'salary_max',
+                'experience', 'age_min', 'age_max', 'gender', 'language', 'skills',
+                'education', 'description']
+        data_row = [data['title'], url, data['company_name'], data['company_link'], data['salary'][0], data['salary'][1],
+                    data['experience'], data['age'][0], data['age'][1], data['gender'], data['language'],
+                    ' , '.join(data['skills']), ' , '.join(data['education']), data['job_description']]
         # Check if csv file not exists or does not have header in it create a new csv file with 'fields' as header
         try:
             f = open(csv_file, 'rt', encoding='utf-8')
